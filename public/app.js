fe10d29a6a84d5ab02d77a6e01a6b0f70dc468b1
abedshamia <ab.shamia96@gmail.com>
2022-03-07 14:43:22 +0200

    Delete function

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 const form = document.getElementById('#form');
-fetch('/users')
+const renderData = fetch('/users')
   .then(res => res.json())
   .then(users => {
     const table = document.getElementById('users-table');
@@ -10,11 +10,29 @@ fetch('/users')
       row.appendChild(name);
       const location = document.createElement('td');
       location.textContent = user.location;
+      const deleteBtn = document.createElement('td');
+      const deleteLink = document.createElement('a');
+      deleteBtn.appendChild(deleteLink);
+      deleteLink.classList.add('delete-btn');
+      deleteLink.textContent = 'Delete';
+
+      deleteLink.href = `/users/${user.id}`;
+
       row.appendChild(location);
+      row.appendChild(deleteBtn);
       table.appendChild(row);
+
+      deleteLink.addEventListener('click', e => {
+        e.preventDefault();
+        window.location.reload();
+        fetch(deleteLink.href, {
+          method: 'DELETE',
+        })
+          .then(res => res.json())
+          .then(data => {
+            console.log(data);
+            location.parentNode.remove();
+          });
+      });
     });
   });
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-});
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 const {getData} = require('../database/queries/getData');
 const {postData} = require('../database/queries/postData');
+const {deleteUser} = require('../database/queries/deleteUser');
 
 router.get('/users', (req, res) => {
   getData().then(data => {
@@ -24,6 +25,19 @@ router.post('/create-user', (req, res) => {
 
   postData(name, location).then(data => {
     res.status(201).redirect('/');
+  });
+});
+
+router.delete('/users/:id', (req, res) => {
+  const id = +req.params.id;
+
+  if (!id) {
+    return res.status(400).json({
+      error: 'Please provide a valid id',
+    });
+  }
+  deleteUser(id).then(data => {
+    res.status(204).redirect('/');
   });
 });
 
